317318536f68584d5bf0831a0a10275a71168c34
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-03 18:12:14 -0800

    vuesax

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
 import router from "./router";
 
 createApp(App)
   .use(router)
   .mount("#app");
+
+Vue.use(Vuesax, {
+  // options here
+})
