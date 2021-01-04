0b5b8a547c84c35c4a0750a12ea46ec13b236cb1
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-03 16:48:04 -0800

    added vuesax, material icons, and router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+import 'material-icons/iconfont/material-icons.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
+
+
+Vue.use(Vuesax, {
+  // options here
+});
