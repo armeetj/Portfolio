09e9a6cdf2f787c6472108bd115da71dffabf9aa
ArmeetJatyani <armeetjatyani@gmail.com>
2021-01-03 18:01:43 -0800

    backing up

---
+++ portfolio/src/views/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contact">
+        <h1>This is an contact page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
