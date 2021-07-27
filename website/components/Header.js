1e099edf773137d5c57b8c3b06af367fb37e8902
ArmeetJatyani <armeetjatyani@gmail.com>
2021-07-26 17:38:36 -0700

    beginning abstraction attempt, navbar

---
+++ website/components/Header.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Header() {
+	return (
+		<Head>
+			<title>Armeet</title>
+			<meta name="description" content="My personal website." />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	);
+}
