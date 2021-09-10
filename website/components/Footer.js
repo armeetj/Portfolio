a020df8e628bfe4a0f2e7d6eb31e70c288c4b99e
ArmeetJatyani <armeetjatyani@gmail.com>
2021-09-09 22:23:51 -0700

    finished navbar with animations

---
+++ website/components/Footer.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer(props) {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+}
