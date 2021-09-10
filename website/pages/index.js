9d6304ea3ef4a74f0a8ec7d0de7ca14b3a38bad7
ArmeetJatyani <armeetjatyani@gmail.com>
2021-09-09 22:23:21 -0700

    test

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,5 +1,6 @@
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div className={styles.container}>
       <Header title="Armeet." description="Hi, I'm Armeet. Check out my personal website!" />
       <Navbar current="home" />
+      <Footer />
     </div>
   )
 }
